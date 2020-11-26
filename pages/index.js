656d5ba6ee82bdd3d11885703fcc74c21d5e16ce
Austin Piel <austin.piel@airbnb.com>
2020-11-26 14:50:05 -0500

    purge css styles only on production

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <div className="bg-black md:container">ASDFAS</div>;
+  console.log(process.env.NODE_ENV);
+  return <div className="mx-auto bg-black md:container">ASDFAS</div>;
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "@fullhuman/postcss-purgecss":
+      process.env.NODE_ENV === "production"
+        ? {
+            content: [
+              "./pages/**/*.tsx",
+              "./pages/*.tsx",
+              "./pages/_document.js",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          }
+        : undefined,
+  },
 };
