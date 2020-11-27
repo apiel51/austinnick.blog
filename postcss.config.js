acff2ac0120c7e4c60a6820cdd170c2a80106a29
Austin Piel <austin.piel@airbnb.com>
2020-11-26 19:56:02 -0500

    fix purge for production

--- postcss.config.js
+++ postcss.config.js
@@ -2,17 +2,17 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // "@fullhuman/postcss-purgecss":
-    //   process.env.NODE_ENV === "production"
-    //     ? {
-    //         content: [
-    //           "./pages/**/*.tsx",
-    //           "./pages/*.tsx",
-    //           "./pages/_document.js",
-    //         ],
-    //         defaultExtractor: (content) =>
-    //           content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    //       }
-    //     : undefined,
+    "@fullhuman/postcss-purgecss":
+      process.env.NODE_ENV === "production"
+        ? {
+            content: [
+              "./pages/**/*.tsx",
+              "./pages/*.tsx",
+              "./pages/_document.js",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          }
+        : undefined,
   },
 };
