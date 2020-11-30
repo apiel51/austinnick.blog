93242bb47fa36c6346de21915ade63a4ad4ddabd
Austin Piel <austin.piel@airbnb.com>
2020-11-30 08:04:33 -0500

    update books

--- components/bookshelf/index.tsx
+++ components/bookshelf/index.tsx
@@ -4,14 +4,14 @@ export default function Bookshelf() {
   return (
     <div className="pb-8">
       <div className="text-sm pb-4 text-gray-700">
-        Last updated on November 28, 2020
+        Last updated on November 30, 2020
       </div>
       This is a running list of books that I've read since the inception of this
       website.
       <div>
         <BookRow
           title="Zero to One: Notes on Startups, or How to Build the Future"
-          date="11/29/20"
+          date="11/30/20"
         />
       </div>
     </div>
