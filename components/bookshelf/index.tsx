bfcd5e9eafd88fc81b9139aa07e6c9ec771b29e0
Austin Piel <austin.piel@airbnb.com>
2020-12-08 16:25:53 -0500

    Remove updated at from Bookshelf

--- components/bookshelf/index.tsx
+++ components/bookshelf/index.tsx
@@ -3,9 +3,6 @@ import BookRow from "./BookRow";
 export default function Bookshelf() {
   return (
     <div className="pb-8">
-      <div className="text-sm pb-4 text-gray-700">
-        Last updated on November 30, 2020
-      </div>
       This is a running list of books that I've read since the inception of this
       website.
       <div>
