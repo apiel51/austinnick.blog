57b8953186ec482fdc8f156d06612dc4b38295a0
Austin Piel <apiel51@gmail.com>
2021-01-10 15:23:05 -0800

    Update about description

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -3,15 +3,37 @@ export default function About() {
     <>
       <div>Hi! I'm Austin 👋</div>
       <div className="pt-4">
-        I live in Seattle and develop tooling for trust agents at Airbnb.
-        Previously, I studied Computer Science at University of Maryland College
-        Park.
+        I live in Seattle and work as a software engineer at{" "}
+        <a
+          aria-label="Explo link"
+          className="underline"
+          href="https://explo.co/"
+          target="_blank"
+        >
+          Explo
+        </a>
+        . Previously, I worked on trust agent tooling at{" "}
+        <a
+          aria-label="Airbnb link"
+          className="underline"
+          href="https://www.airbnb.com/"
+          target="_blank"
+        >
+          Airbnb
+        </a>
+        . Prior to that, I studied Computer Science at University of Maryland
+        College Park.
       </div>
       <div className="pt-4">
         Professionally, I'm particularly interested in entrepreneurship and
         anything to do with frontend development (backend is kinda cool too).
         I'm currently building{" "}
-        <a className="underline" href="https://www.momo.credit/">
+        <a
+          aria-label="momo link"
+          className="underline"
+          href="https://www.momo.credit/"
+          target="_blank"
+        >
           momo
         </a>
         , the first dynamic mobile wallet.
