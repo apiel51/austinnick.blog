b3e916dda7494d5993a5ffc6f82c96d62a9f0219
Austin Piel <apiel51@gmail.com>
2021-01-12 21:22:12 -0800

    Fix production build

--- pages/writing/[pid].tsx
+++ pages/writing/[pid].tsx
@@ -1,16 +1,20 @@
 import { useRouter } from "next/router";
 
 import Page from "components/shared/Page";
-import posts from "components/writing/posts";
+import { Post1 } from "components/writing/posts";
 
 export default function Post() {
   const router = useRouter();
   const { pid } = router.query;
-  const CurrentPost = posts[Number(pid) - 1];
 
-  return (
-    <Page>
-      <CurrentPost />
-    </Page>
-  );
+  switch (Number(pid)) {
+    case 1:
+      return (
+        <Page>
+          <Post1 />
+        </Page>
+      );
+    default:
+      return null;
+  }
 }
