8ca422624d16963f3a5f1f98737d622f630d3374
Austin Nick Piel <apiel51@gmail.com>
2023-01-29 20:28:57 -0500

    Add spotify redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@
  */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/spotify',
+        destination:
+          'https://open.spotify.com/playlist/4gg46IwqAX0UlwcaS0dzJf?si=4db47841c1a34590',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
